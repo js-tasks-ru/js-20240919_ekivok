672b5af2773cac2eb390a6a2b4aa9e0c8bc1786c
yokne <sofya.rusakowa@yandex.ru>
2024-09-28 23:41:51 +0300

    task 3.1 - createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,11 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+
+export function createGetter(path) {
+  return (obj) => {
+    const keys = path.split(".");
+    return keys.reduce((acc, key)=> acc?.hasOwnProperty(key) ? acc[key] : undefined, obj);  
+  };
 }
