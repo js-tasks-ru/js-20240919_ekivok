626e637dd3c4b25c89e289fc416780a718ae4d0b
yokne <sofya.rusakowa@yandex.ru>
2024-09-29 13:45:50 +0300

    task 3.3 - trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,19 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
-
+export function trimSymbols(string, size = null) {
+  if (size === null) {return string;} 
+  if (size === 0) {return '';}
+  let trimmedString = '';
+  let counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string?.[i + 1]) {counter++;} 
+    if (counter === size) {
+      counter = 0;
+      continue;
+    } else {
+      trimmedString += string[i];
+    }
+  }
+  return trimmedString;
 }
